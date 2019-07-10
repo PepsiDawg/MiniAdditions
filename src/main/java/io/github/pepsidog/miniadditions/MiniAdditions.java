b57dce50522d21cd03c2295a4f1d3e5101c3c0f1
Josh <mrjoshuasperry@gmail.com>
2019-07-10 05:32:04 -0400

    Add Auto Stack

--- src/main/java/io/github/pepsidog/miniadditions/MiniAdditions.java
+++ src/main/java/io/github/pepsidog/miniadditions/MiniAdditions.java
@@ -2,6 +2,7 @@ package io.github.pepsidog.miniadditions;
 
 import com.google.common.collect.Lists;
 import io.github.pepsidog.miniadditions.additions.armorstands.ArmorStandAdditions;
+import io.github.pepsidog.miniadditions.additions.autostack.AutoStackListener;
 import io.github.pepsidog.miniadditions.additions.biomebombs.BiomeBombListener;
 import io.github.pepsidog.miniadditions.additions.chatitem.ChatItemListener;
 import io.github.pepsidog.miniadditions.additions.cobblegenerator.CobbleGeneratorListener;
@@ -50,12 +51,12 @@ public class MiniAdditions extends JavaPlugin {
         saveDefaultConfig();
 
         ArrayList<String> names = Lists.newArrayList(
-                "ArmorStands",     "BiomeBombs",           "ChatItem",
-                "CobbleGenerator", "CompressedMobs",       "ConcreteMixer",
-                "CraftingKeeper",  "Creeperworks",         "EasyPaintings",
-                "Experimental",    "ExperimentalCommands", "IgneousGenerator",
-                "ImprovedShears",  "NamePing",             "SlimyBoots",
-                "WoodPile"
+                "ArmorStands",      "AutoStack",       "BiomeBombs",
+                "ChatItem",         "CobbleGenerator", "CompressedMobs",
+                "ConcreteMixer",    "CraftingKeeper",  "Creeperworks",
+                "EasyPaintings",    "Experimental",    "ExperimentalCommands",
+                "IgneousGenerator", "ImprovedShears",  "NamePing",
+                "SlimyBoots",       "WoodPile"
         );
         ConfigManager.Initialize(this, names);
         for (String name : names) {
@@ -93,6 +94,11 @@ public class MiniAdditions extends JavaPlugin {
     public void initArmorStands() {
         this.getServer().getPluginManager().registerEvents(new ArmorStandAdditions(), this);
         this.getLogger().info("Armor Stands enabled");
+    }
+
+    public void initAutoStack() {
+        this.getServer().getPluginManager().registerEvents(new AutoStackListener(), this);
+        this.getLogger().info("Auto Stack enabled");
     }
 
     public void initBiomeBombs() {
